1f677d1be827d3bfbf289fe3f1b29f2ec6820a65
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-03-03 09:58:35 -0600

    serving a single route

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('hello?')
+})
+
+server.listen(PORT, () => {
+  console.log("HEY, You've got a running server on port ", PORT);
+});
